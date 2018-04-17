552e035144c47f75b7a344f34bcd8040c9a7303d
ryomak <ryooomaaa0413@outlook.com>
2018-04-17 16:36:28 +0900

    add transmitter

---
+++ datatype/datatype.go
@@ -0,0 +1,7 @@
+package datatype
+
+
+type Response struct {
+	Header map[string][]string
+	Body string
+}
--- src/transmitter/transmitter.go
+++ src/transmitter/transmitter.go
@@ -2,12 +2,42 @@ package transmitter
 
 import (
 	"net/http"
+	"io/ioutil"
+	"strings"
+	"github.com/ryomak/browser/datatype"
 )
 
-func Transmit(url string) (string, error) {
-	_, err := http.Get(url)
-	if err != nil {
-		return "", err
+func Transmit(url string) (*datatype.Response, error) {
+	if strings.Index("http",url) != -1 {
+		return GetLocal(url)
 	}
-	return "jeu", nil
+	return Fetch(url)
+}
+
+func GetLocal(path string)(*datatype.Response,error){
+	body ,err := ioutil.ReadFile(path)
+	if err != nil{
+		return &datatype.Response{},err
+	}
+	res := &datatype.Response{
+		Body:string(body),
+	}
+	return res,nil
+}
+
+func Fetch(url string)(*datatype.Response, error){
+	resp,err := http.Get(url)
+	if err != nil {
+		return &datatype.Response{},err
+	}
+	defer resp.Body.Close()
+	body ,err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return &datatype.Response{},err
+	}
+	res := &datatype.Response{
+		Header:resp.Header,
+		Body:string(body),
+	}
+	return res ,nil
 }
